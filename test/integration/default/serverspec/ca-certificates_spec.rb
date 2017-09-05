3879c64d7cb35f96c79aa5850d88bfe75e074c00
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2017-09-05 21:46:02 +0100

    Fix handler for CentOS/RHEL to include "extract"
    Improve tests to verify the certificate as well

--- test/integration/default/serverspec/ca-certificates_spec.rb
+++ test/integration/default/serverspec/ca-certificates_spec.rb
@@ -6,8 +6,13 @@ Serverspec.describe 'ca-certificates' do
     it { should be_installed }
   end
 
+  describe command("/usr/bin/update-ca-trust check"), :if => (os[:family] == 'redhat' and host_inventory[:platform_version] < '7.0') do
+    let(:disable_sudo) { true }
+    its(:stdout) { should match /PEM.*Status.*ENABLED/ }
+  end
+
   describe "x509 example certificate" do
-    let(:ca_certificates_dir) {
+    let(:ca_certificates_local_dir) {
       case os[:family]
       when 'redhat','fedora'
         return '/etc/pki/ca-trust/source/anchors'
@@ -17,17 +22,29 @@ Serverspec.describe 'ca-certificates' do
         return '/usr/local/share/ca-certificates'
       end
     }
+    let(:ca_certificates_bundle) {
+      case os[:family]
+      when 'redhat','fedora'
+        return '/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt'
+      when 'debian'
+        return '/etc/ssl/certs/ca-certificates.crt'
+      when 'ubuntu'
+        return '/etc/ssl/certs/ca-certificates.crt'
+      end
+    }
 
-    subject! { x509_certificate("#{ca_certificates_dir}/www.example.com.crt") }
+    subject! { x509_certificate("#{ca_certificates_local_dir}/www.example.com.crt") }
 
     it { should be_certificate }
     its(:subject) { should eq('/C=UK/ST=England/L=London/O=Example Org/OU=Org/CN=www.example.com') }
+
+    describe "verify certificates" do
+      it "should openssl verify certificate" do
+        expect( command("openssl verify -CAfile #{ca_certificates_bundle} #{ca_certificates_local_dir}/www.example.com.crt").stdout).to match /: OK$/
+      end
+    end
   end
 
-  describe command("/usr/bin/update-ca-trust check"), :sudo => false, :if => (os[:family] == 'redhat' and host_inventory[:platform_version] < '7.0') do
-    let(:disable_sudo) { true }
-    its(:stdout) { should match /PEM.*Status.*ENABLED/ }
-  end
   # RedHat 7+ doesn't have 'check'
   # Debian's ca-certificates is enabled if installed
 end
